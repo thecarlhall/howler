c0d171ee0f129a210c7c774544ddb89b4c172d95
Carl Hall <carl@hallwaytech.com>
2011-12-13 01:14:35 -0500

    Better handling of repeat data.

--- public/assets/js/dataload.js
+++ public/assets/js/dataload.js
@@ -9,17 +9,24 @@ howler.dataload.lastOuput = null;
 howler.dataload.pollUntilDone = function() {
 	var input = { 'dir': $('#form_dir').val(), 'max': 25, 'update': $('#form_update').attr('checked') };
 	$.post('dataload', input, function(data) {
-		if (data != howler.dataload.lastOutput) {
-			$('#output').html($('#output').html() + data);
-			start = data.indexOf('Processed ') + 'Processed '.length;
-			end = data.indexOf(" entries");
+		$('#output').html($('#output').html() + data);
+
+		processedPos = data.lastIndexOf('Processed ');
+
+		dataNoSummary = data.substring(0, processedPos);
+		if (dataNoSummary != howler.dataload.lastOutput) {
+			howler.dataload.lastOutput = dataNoSummary;
+
+			start = processedPos + 'Processed '.length;
+			end = data.lastIndexOf(" entries");
 			count = data.substring(start, end);
+
 			if (parseInt(count) > 0) {
 				setTimeout('howler.dataload.pollUntilDone()', 10);
 			}
 		} else {
 			alert('Data looks the same as last time. Are we looping on something?');
+			howler.dataload.lastOutput = null;
 		}
 	});
 };
-
