abaa99daeb9cf2f6b43ee442dbfa757935d5b1ea
Carl Hall <carl@hallwaytech.com>
2012-01-08 23:32:17 -0500

    Added encoding for name in url.

--- fuel/app/views/entries/view.php
+++ fuel/app/views/entries/view.php
@@ -4,9 +4,9 @@ Lang::load('howler');
 <span class='title'><?php echo $entry->title ?></span> 
 <?php echo Lang::get('by') ?> 
 <a title="<?php echo Lang::get('find_by', array('type' => 'artist', 'name' => $entry->artist)) ?>"
-	href="#artist=<?php echo $entry->artist ?>"
+	href="#artist=<?php echo urlencode($entry->artist) ?>"
 	><span class='artist'><?php echo $entry->artist ?></span></a> 
 <?php echo Lang::get('from') ?> 
 <a title="<?php echo Lang::get('find_by', array('type' => 'album', 'name' => $entry->album)) ?>"
-	href="#album=<?php echo $entry->album ?>"
+	href="#album=<?php echo urlencode($entry->album) ?>"
 	><span class='album'><?php echo $entry->album ?></span></a> 
