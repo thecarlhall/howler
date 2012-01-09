1c650543281214b024634a58146390d0839fb046
Carl Hall <carl@hallwaytech.com>
2012-01-08 22:52:46 -0500

    Added searching via entry view.

--- fuel/app/views/entries/view.php
+++ fuel/app/views/entries/view.php
@@ -3,6 +3,10 @@ Lang::load('howler');
 ?>
 <span class='title'><?php echo $entry->title ?></span> 
 <?php echo Lang::get('by') ?> 
-<span class='artist'><?php echo $entry->artist ?></span> 
+<a title="<?php echo Lang::get('find_by', array('type' => 'artist', 'name' => $entry->artist)) ?>"
+	href="#artist=<?php echo $entry->artist ?>"
+	><span class='artist'><?php echo $entry->artist ?></span></a> 
 <?php echo Lang::get('from') ?> 
-<span class='album'><?php echo $entry->album ?></span> 
+<a title="<?php echo Lang::get('find_by', array('type' => 'album', 'name' => $entry->album)) ?>"
+	href="#album=<?php echo $entry->album ?>"
+	><span class='album'><?php echo $entry->album ?></span></a> 
